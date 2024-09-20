46d8941c0d08f05161f5ddb61b8110b89c992ee9
Arthur Molina <arthurmolina@gmail.com>
2024-09-19 22:15:26 -0300

    Fix changes to 23.10

--- install/check-version.sh
+++ install/check-version.sh
@@ -3,16 +3,18 @@
 if [ ! -f /etc/os-release ]; then
     echo "$(tput setaf 1)Error: Unable to determine OS. /etc/os-release file not found."
     echo "Installation stopped."
+    pause
     exit 1
 fi
 
 . /etc/os-release
 
 # Check if running on Ubuntu 24.04 or higher
-if [ "$ID" != "ubuntu" ] || [ $(echo "$VERSION_ID >= 24.04" | bc) != 1 ]; then
+if [ "$ID" != "ubuntu" ] || [ $(echo "$VERSION_ID >= 23.10" | bc) != 1 ]; then
     echo "$(tput setaf 1)Error: OS requirement not met"
     echo "You are currently running: $ID $VERSION_ID"
-    echo "OS required: ubuntu 24.04 or higher"
+    echo "OS required: ubuntu 23.10 or higher"
     echo "Installation stopped."
+    pause
     exit 1
 fi
